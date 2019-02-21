ab8b47d3e3432a6bc36eb727f2a915259fbf61fb
Madalina <maddiemihailescu@yahoo.com>
2019-02-21 21:40:43 +0200

    Curs 1

---
+++ Exemplu4.java
@@ -0,0 +1,16 @@
+public class Exemplu4 {
+    
+    public static void main(String [] args) {
+        String s1 = "Ana are " + 3 + 4 + " mere";
+        System.out.println(s1);
+        
+        String s2 = "Ana are " + (3 + 4) + " mere";
+        System.out.println(s2);
+        
+        String s3 = 3 + 4 + " mere are Ana";
+        System.out.println(s3);
+        
+        int x = -10;
+        int y = -x;
+    }
+}
---
+++ Exemplu7.java
@@ -0,0 +1,14 @@
+/**
+ * Comentariul
+ * regege
+ * gegefw
+ */
+public class Exemplu7 {
+ 
+    public static void main(String[] args) {
+        String x = 3 > 6 ? "Hello" : "World";
+        
+        System.out.println(x);
+        
+    }
+}
